01b21e1530d2aae4276cf0a0fe24778782984dba
kaki5507 <ckdbioksh@naver.com>
2024-04-29 20:19:43 +0900

    test(상품명 조회 테스트)  - 리스트로 테스트 상품 JPA 사용해서 조회

--- src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -50,7 +50,7 @@ class ItemRepositoryTest {
     @Test
     @DisplayName("상품명 조회 테스트")
     public void findByItemNmTest(){
-        this.createItemTest();
+        this.createItemList();
         List<Item> itemList = itemRepository.findByItemNm("테스트 상품1");
         for(Item item : itemList){
             System.out.println(item.toString());
