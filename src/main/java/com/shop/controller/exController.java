f18a1a072c864d1c20d8c9b89891be82af879440
kaki5507 <ckdbioksh@naver.com>
2024-05-05 15:24:03 +0900

    test(thymleaf)  - 타임리프 문법 테스트 및 레이아웃 의존성 추가

--- src/main/java/com/shop/controller/exController.java
+++ src/main/java/com/shop/controller/exController.java
@@ -1,10 +1,15 @@
 package com.shop.controller;
 
+import com.shop.dto.ItemDto;
 import lombok.extern.java.Log;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "/ex")
@@ -12,8 +17,31 @@ public class exController {
 
     @GetMapping(value = "/ex01")
     public String ex01(Model model){
-        model.addAttribute("data", "타임리프");
+        ItemDto itemDto = new ItemDto();
+        itemDto.setItemDetail("상품 상셋 설명");
+        itemDto.setItemNm("테스트 상품1");
+        itemDto.setPrice(10000);
+        itemDto.setRegTime(LocalDateTime.now());
 
+        model.addAttribute("itemDto", itemDto);
         return "ex/ex01";
+    }
+
+    @GetMapping(value = "/ex02")
+    public String ex02(Model model){
+
+        List<ItemDto> itemDtoList = new ArrayList<>();
+
+        for(int i=0; i<=10; i++){
+            ItemDto itemDto = new ItemDto();
+            itemDto.setItemDetail("상품 상셋 설명" + i);
+            itemDto.setItemNm("테스트 상품1" + i);
+            itemDto.setPrice(1000 + i);
+            itemDto.setRegTime(LocalDateTime.now());
+            itemDtoList.add(itemDto);
+        }
+
+        model.addAttribute("itemList", itemDtoList);
+        return "ex/ex02";
     }
 }
