2750f550d74520c85c92705243ad4629edfbcfd6
kaki5507 <ckdbioksh@naver.com>
2024-05-09 21:58:00 +0900

    feat(validation) - 유효성 검증 추가 - 유효성 에러 있으면 bindingResult로 담아서 다시 회원가입 페이지로 이동

--- src/main/java/com/shop/dto/MemberFormDto.java
+++ src/main/java/com/shop/dto/MemberFormDto.java
@@ -1,13 +1,28 @@
 package com.shop.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.logging.log4j.core.config.plugins.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.Length;
 
 @Getter
 @Setter
 public class MemberFormDto { // 가입정보 담는 DTO
+
+    @NotBlank(message = "이름은 필수 입력 값.")
     private String name;
+
+    @NotEmpty(message = "이메일은 필수 입력 값.")
+    @Email(message = "이메일 형식으로 입력해주세요.")
     private String email;
+
+    @NotEmpty(message = "이메일은 필수 입력 값.")
+    @Length(min = 8, max = 16, message = "비밀번호 8자 이상 , 16자 이하")
     private String password;
+
+    @NotEmpty(message = "주소 필수 입력 값.")
     private String address;
+
 }
