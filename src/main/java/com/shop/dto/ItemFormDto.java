31ec7792f3cf8ce78094e4ba3653f6a47fc999a5
kaki5507 <ckdbioksh@naver.com>
2024-05-26 17:36:25 +0900

    feat(ItemImg,ItemFormDto) - 상품 이미지 엔티티 생성 - 상품 이미지 DTO 생성 - 상품 데이터 DTO 생성

---
+++ src/main/java/com/shop/dto/ItemFormDto.java
@@ -0,0 +1,47 @@
+package com.shop.dto;
+
+import com.shop.constant.ItemSellStatus;
+import com.shop.entity.Item;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.logging.log4j.core.config.plugins.validation.constraints.NotBlank;
+import org.modelmapper.ModelMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter @Setter
+public class ItemFormDto {
+
+    private Long id;
+
+    @NotBlank(message = "상품명은 필수 입력 값입니다.")
+    private String itemNm;
+
+    @NotBlank(message = "가격은 필수 입력 값입니다.")
+    private Integer price;
+
+    @NotBlank(message = "이름은 필수 입력 값입니다.")
+    private String itemDetail;
+
+    @NotNull(message = "재고는 필수 입력 값입니다.")
+    private Integer stockNumber;
+
+    private ItemSellStatus itemSellStatus;
+
+    private List<ItemImgDto> itemImgDtoList = new ArrayList<>();
+
+    private List<Long> itemImgIds = new ArrayList<>();
+
+    private static ModelMapper modelMapper = new ModelMapper();
+
+    public Item creaItem(){
+        return modelMapper.map(this, Item.class); // Item 클래스 모델에 담어서 생성
+    }
+
+    public static ItemFormDto of(Item item){
+        return modelMapper.map(item,ItemFormDto.class);
+    }
+
+}
--- src/main/java/com/shop/entity/BaseEntity.java
+++ src/main/java/com/shop/entity/BaseEntity.java
@@ -9,8 +9,8 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-@EntityListeners(value = {AuditingEntityListener.class})
-@MappedSuperclass
+@EntityListeners(value = {AuditingEntityListener.class}) // AuditingClass가 감지하기 위해
+@MappedSuperclass // 공통 필드를 받아서 여러 엔티티 객체에 넣어줄 수 있음
 @Getter @Setter
 public abstract class BaseEntity extends BaseTimeEntity{
 
